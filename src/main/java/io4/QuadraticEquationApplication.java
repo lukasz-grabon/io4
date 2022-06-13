22ada19f42b3c6b4a1ad6609c1b6ede4f93de326
Ewa Makowska <makowskaewa@o2.pl>
2022-06-13 22:47:49 +0200

    final run

--- src/main/java/io4/QuadraticEquationApplication.java
+++ src/main/java/io4/QuadraticEquationApplication.java
@@ -4,6 +4,6 @@ public class QuadraticEquationApplication {
 
 
     public static void main(String[] args){
-
+        System.out.println(new QuadraticEquation(2d, 1d, 2d).getResultMessage());
     }
 }
