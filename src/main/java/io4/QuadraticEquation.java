5b4849d963c009d34cf75a8f2a96b7cf32783a8b
Ewa Makowska <makowskaewa@o2.pl>
2022-06-13 19:11:52 +0200

    delta calculation

--- src/main/java/io4/QuadraticEquation.java
+++ src/main/java/io4/QuadraticEquation.java
@@ -2,7 +2,57 @@ package io4;
 
 public class QuadraticEquation {
 
-    public float countDelta(float a, float b, float c){
+    private double a, b, c, delta;
+    private String resultMessage;
 
+    public QuadraticEquation(double a, double b, double c) {
+        setA(a);
+        setB(b);
+        setC(c);
+        countDelta();
+    }
+
+    private void countDelta(){
+        setDelta(Math.pow(getB(), 2) - 4 * getA() * getC());
+    }
+
+    public String getResultMessage() {
+        return resultMessage;
+    }
+
+    public void setResultMessage(String resultMessage) {
+        this.resultMessage = resultMessage;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public double getDelta() {
+        return delta;
+    }
+
+    public void setDelta(double delta) {
+        this.delta = delta;
     }
 }
