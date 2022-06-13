02b933880567aa8786ca4c71e08c1001cf3a6fa7
Ewa Makowska <makowskaewa@o2.pl>
2022-06-13 19:10:47 +0200

    upgrade test pt.4

--- src/test/java/io4/QuadraticEquationTest.java
+++ src/test/java/io4/QuadraticEquationTest.java
@@ -1,13 +1,12 @@
 package io4;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class QuadraticEquationTest {
 
@@ -16,25 +15,25 @@ class QuadraticEquationTest {
                 Arguments.of(1d, 2d, 1d, 0d, -1d, null, null, "Delta równa 0, pierwiastek x0 = -1.0"),
                 Arguments.of(2d, 1d, 2d, 4d, null, null, null, "Delta mniejsza od zera, brak pierwiastków"),
                 Arguments.of(1d, 4d, 3d, 4d, null, -1d, -3d, "Delta wieksza od zera, pierwiastki: x1 = -1.0, x2 = -3.0"),
-                Arguments.of(null, 3d, 1d, null, null, null, null, "Delta wieksza od zera, pierwiastki: x1 = 2, x2 = -2"),
-                Arguments.of(1d, null, 1d, null, null, null, null, "Delta wieksza od zera, pierwiastki: x1 = 2, x2 = -2"),
-                Arguments.of(1d, 3d, null, null, null, null, null, "Delta wieksza od zera, pierwiastki: x1 = 2, x2 = -2")
+                Arguments.of(null, 3d, 1d, null, null, null, null, null),
+                Arguments.of(1d, null, 1d, null, null, null, null, null),
+                Arguments.of(1d, 3d, null, null, null, null, null, null)
         );
     }
 
     @ParameterizedTest
     @MethodSource("provideData")
-    public void testResult(Double a, Double b, Double c, Double delta, Double x0, Double x1, Double x2, String resultMessage){
+    public void testResult(Double a, Double b, Double c, Double delta, Double x0, Double x1, Double x2, String resultMessage) {
         //for
 
         //when
         QuadraticEquation equation = new QuadraticEquation(a, b, c);
 
         //then
-        assertEquals(resultMessage, equation.getResultMessage());
         assertEquals(delta, equation.getDelta());
         assertEquals(x0, equation.getX0());
         assertEquals(x1, equation.getX1());
         assertEquals(x2, equation.getX2());
+        assertEquals(resultMessage, equation.getResultMessage());
     }
 }
