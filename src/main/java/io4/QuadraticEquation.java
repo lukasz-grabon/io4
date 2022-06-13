c16835fbcc22762b6098baae8c577f053df6f209
Ewa Makowska <makowskaewa@o2.pl>
2022-06-13 22:17:30 +0200

    adjust tests and implement missing methods

--- src/main/java/io4/QuadraticEquation.java
+++ src/main/java/io4/QuadraticEquation.java
@@ -2,7 +2,9 @@ package io4;
 
 public class QuadraticEquation {
 
-    private double a, b, c, delta;
+    private double a, b, c;
+    private Double delta, x0, x1, x2;
+    private final StringBuilder STRING_BUILDER = new StringBuilder();
     private String resultMessage;
 
     public QuadraticEquation(double a, double b, double c) {
@@ -10,10 +12,36 @@ public class QuadraticEquation {
         setB(b);
         setC(c);
         countDelta();
+        if (getDelta() == 0) {
+            countX0();
+            setResultMessage(STRING_BUILDER.append("Delta równa 0, pierwiastek x0 = ").append(getX0()).toString());
+        }
+
+        if (getDelta() > 0) {
+            countX1();
+            countX2();
+            setResultMessage(STRING_BUILDER.append("Delta wieksza od zera, pierwiastki: x1 = ").append(getX1()).append(", x2 = ").append(getX2()).toString());
+        }
+
+        if (getDelta() < 0) {
+            setResultMessage(STRING_BUILDER.append("Delta mniejsza od zera, brak pierwiastków").toString());
+        }
     }
 
-    private void countDelta(){
+    private void countDelta() {
         setDelta(Math.pow(getB(), 2) - 4 * getA() * getC());
+    }
+
+    private void countX0() {
+        setX0(-getB() / (2 * getA()));
+    }
+
+    private void countX1() {
+        setX1(-getB() - Math.sqrt(getDelta()) / (2 * getA()));
+    }
+
+    private void countX2() {
+        setX2(-getB() + Math.sqrt(getDelta()) / (2 * getA()));
     }
 
     public String getResultMessage() {
@@ -24,7 +52,7 @@ public class QuadraticEquation {
         this.resultMessage = resultMessage;
     }
 
-    public double getA() {
+    public Double getA() {
         return a;
     }
 
@@ -32,7 +60,7 @@ public class QuadraticEquation {
         this.a = a;
     }
 
-    public double getB() {
+    public Double getB() {
         return b;
     }
 
@@ -40,7 +68,7 @@ public class QuadraticEquation {
         this.b = b;
     }
 
-    public double getC() {
+    public Double getC() {
         return c;
     }
 
@@ -48,11 +76,35 @@ public class QuadraticEquation {
         this.c = c;
     }
 
-    public double getDelta() {
+    public Double getDelta() {
         return delta;
     }
 
     public void setDelta(double delta) {
         this.delta = delta;
+    }
+
+    public Double getX0() {
+        return x0;
+    }
+
+    public void setX0(double x0) {
+        this.x0 = x0;
+    }
+
+    public Double getX1() {
+        return x1;
+    }
+
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    public Double getX2() {
+        return x2;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
     }
 }
--- src/test/java/io4/QuadraticEquationTest.java
+++ src/test/java/io4/QuadraticEquationTest.java
@@ -7,13 +7,14 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class QuadraticEquationTest {
 
     private static Stream<Arguments> provideData() {
         return Stream.of(
                 Arguments.of(1d, 2d, 1d, 0d, -1d, null, null, "Delta równa 0, pierwiastek x0 = -1.0"),
-                Arguments.of(2d, 1d, 2d, 4d, null, null, null, "Delta mniejsza od zera, brak pierwiastków"),
+                Arguments.of(2d, 1d, 2d, -15d, null, null, null, "Delta mniejsza od zera, brak pierwiastków"),
                 Arguments.of(1d, 4d, 3d, 4d, null, -1d, -3d, "Delta wieksza od zera, pierwiastki: x1 = -1.0, x2 = -3.0"),
                 Arguments.of(null, 3d, 1d, null, null, null, null, null),
                 Arguments.of(1d, null, 1d, null, null, null, null, null),
@@ -31,9 +32,21 @@ class QuadraticEquationTest {
 
         //then
         assertEquals(delta, equation.getDelta());
-        assertEquals(x0, equation.getX0());
-        assertEquals(x1, equation.getX1());
-        assertEquals(x2, equation.getX2());
+        if(x0 == null){
+            assertNull(equation.getX0());
+        }else {
+            assertEquals(x0, equation.getX0());
+        }
+        if(x1 == null){
+            assertNull(equation.getX1());
+        }else {
+            assertEquals(x1, equation.getX1());
+        }
+        if(x2 == null){
+            assertNull(equation.getX2());
+        }else {
+            assertEquals(x2, equation.getX2());
+        }
         assertEquals(resultMessage, equation.getResultMessage());
     }
 }
